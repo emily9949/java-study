ec304f8032b5bf467f787f500b2f4edafbf80135
Sumin <emily9949@naver.com>
2025-01-25 23:44:24 +0900

    디렉토리명 변경

--- chap05-class-and-object-lecture-source/src/com/sumin/section02/encapsulation/problem2/Application.java
+++
@@ -1,9 +0,0 @@
-package com.sumin.section02.encapsulation.problem2;
-
-public class Application {
-    public static void main(String[] args) {
-        Monster monster1 = new Monster();
-        monster1.name = "드라큘라";
-        monster1.setInfo2(200);
-    }
-}
